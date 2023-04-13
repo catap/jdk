catap/jdk

commit da43cb5e463069cf4dafb262664f0d3d7c2e0eac

8224957: C2 compilation fails with assert: Bad graph detected in build_loop_late
Disable walking up the dominator chain for regions with more than 2 input paths.

Reviewed-by: roland, kvn

src/hotspot/share/opto/node.cpp
@@ -1228,12 +1228,10 @@ bool Node::dominates(Node* sub, Node_List &nlist) {
     if (sub == up && sub->is_Loop()) {
       // Take loop entry path on the way up to 'dom'.
       up = sub->in(1); // in(LoopNode::EntryControl);
-    } else if (sub == up && sub->is_Region() && sub->req() != 3) {
-      // Always take in(1) path on the way up to 'dom' for clone regions
-      // (with only one input) or regions which merge > 2 paths
-      // (usually used to merge fast/slow paths).
+    } else if (sub == up && sub->is_Region() && sub->req() == 2) {
+      // Take in(1) path on the way up to 'dom' for regions with only one input
       up = sub->in(1);
-    } else if (sub == up && sub->is_Region()) {
+    } else if (sub == up && sub->is_Region() && sub->req() == 3) {
       // Try both paths for Regions with 2 input paths (it may be a loop head).
       // It could give conservative 'false' answer without information
       // which region's input is the entry path.

src/hotspot/share/opto/c2_globals.hpp
@@ -510,7 +510,7 @@
           "Sets max value cached by the java.lang.Integer autobox cache")   \
           range(0, max_jint)                                                \
                                                                             \
-  experimental(bool, AggressiveUnboxing, false,                             \
+  experimental(bool, AggressiveUnboxing, true,                              \
           "Control optimizations for aggressive boxing elimination")        \
                                                                             \
   develop(bool, TracePostallocExpand, false, "Trace expanding nodes after"  \

src/hotspot/share/opto/memnode.cpp
@@ -1435,8 +1435,6 @@ Node *LoadNode::split_through_phi(PhaseGVN *phase) {
     }
   }
 
-  bool load_boxed_phi = load_boxed_values && base_is_phi && (base->in(0) == mem->in(0));
-
   // Split through Phi (see original code in loopopts.cpp).
   assert(C->have_alias_type(t_oop), "instance should have alias type");
 

test/hotspot/jtreg/compiler/eliminateAutobox/TestSplitThroughPhi.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8224957
+ * @summary Test splitting a boxed field load through a loop Phi.
+ * @run main/othervm -XX:-TieredCompilation -XX:-ProfileInterpreter
+ *                   -XX:CompileOnly=compiler.eliminateAutobox.TestSplitThroughPhi::test
+ *                   compiler.eliminateAutobox.TestSplitThroughPhi
+ */
+
+package compiler.eliminateAutobox;
+
+public class TestSplitThroughPhi {
+
+  static volatile boolean loop = true;
+
+  public static void test(Object obj) {
+      for (int i = 0; i == 0; ) {
+          // OSR compilation generates Region with 3 input paths
+          while (true) {
+              // Load from boxed int
+              int val = (Integer)obj;
+              if (val == i++) break;
+              if (!loop) break;
+          }
+      }
+  }
+
+  public static void main(String[] args) {
+      test(100_000);
+  }
+}

commit 6b1e05509030bc59af8b7c5b6f63427b33832cd5

Use full dominant search for regions
This is a fix for the regression introduced by
da43cb5e463069cf4dafb262664f0d3d7c2e0eac in fix 8224957.

This regression was found while attempting to migrate an application
from JDK 1.8 to JDK 17, by running internal benchmarks, and while
investigating abnormal memory usage for about 4 times more from one of
them.

The regression appears in the JMH benchmark, which builds a huge tree
which contains boxed integers from 0 to a few thousand. A tree has very
complex structure and the same objects are reused a lot.

When an `integer` is found it's collected as `Integer` and unboxed
inside the collector callback.

This benchmark was run with `ParallelGC` on different JVMs: `JDK
1.8.0_362`, `JDK 11.0.18`, `JDK 13.0.13`, `JDK 15.0.9`, `JDK 17.0.6`,
`JDK 19.0.2` and `JDK 20`. This allows to see that something has changed
between 13 and 15, and that the memory footprint for this code has
increased from `3152` to `11828` bytes per operation.

After that I've done a `git bisect` which allows me to locate the
introducer.

So the current fix reduces the memory footprint on the local root
425ef0685c584abec80454fbcccdcc6db6558f93 to `2960` bytes per operation.

src/hotspot/share/opto/node.cpp
@@ -1310,7 +1310,7 @@ bool Node::dominates(Node* sub, Node_List &nlist) {
     } else if (sub == up && sub->is_Region() && sub->req() == 2) {
       // Take in(1) path on the way up to 'dom' for regions with only one input
       up = sub->in(1);
-    } else if (sub == up && sub->is_Region() && sub->req() == 3) {
+    } else if (sub == up && sub->is_Region()) {
       // Try both paths for Regions with 2 input paths (it may be a loop head).
       // It could give conservative 'false' answer without information
       // which region's input is the entry path.